3575e46d82d079112d86c37c2237b40aad07c82e
hrishiksh <hrishi1pathak@gmail.com>
2021-07-29 17:58:33 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79abcf39430030be88355ab339fdd988'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63bf5061-d0fa-401a-93f4-09264a82a871'
+};
